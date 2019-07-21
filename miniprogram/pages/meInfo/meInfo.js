f63903cb43646e12cbac51e35ece4991a6821cf9
wpj <2997261975@qq.com>
2019-07-21 09:39:18 +0800

    complete basic function of meInfo, meInfoEdit, mefollow, mefans

--- miniprogram/pages/meInfo/meInfo.js
+++ miniprogram/pages/meInfo/meInfo.js
@@ -5,62 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    bg:'../../images/bg.png',
+    avatar:'../../images/avatar.png',
+    name:'小明',
+    sex:'../../images/male.png',      //0表示女，1表示男
+    fans_num:3,
+    follow_num:3,
+    poster_num:3,
+    sign:'软件学院学生',
+    age:19,
+    birth:'2000年1月1号',
+    region:'广州'
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  editInfo:function(e){
+    wx.navigateTo({
+      url: '../meInfoEdit/meInfoEdit'
+    })
   }
 })

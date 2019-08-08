de409be52b58c53439cd2db575e409363fffd78b
trj <trj@example.com>
2019-08-08 12:44:58 +0800

    完成发帖页面，回复页面的点赞功能处理中...

--- miniprogram/pages/reply/reply.js
+++ miniprogram/pages/reply/reply.js
@@ -9,20 +9,7 @@ const db = wx.cloud.database();
      * 页面的初始数据
      */
     data: {
-      isreplay:false,
-      iscollect:false,
-      page: 0,
-      totalCount: 0,
-      topics: { },
-      content:'',
-      list:{
-      head:'',
-      name:'',
-      date:'',
-      images:'',
-      title:'',
-      content:'',
-      },
+  
       test: [{
         head: '../../images/tian/head.png',
         name: '帖子作者名',
@@ -31,27 +18,15 @@ const db = wx.cloud.database();
         title: '标题1',
         content:'随便加的一些内容'
     },
-        {
-          head: '../../images/tian/head.png',
-          name: '帖子作者名',
-          date: '两天前',
-          images: '../../images/bg.png',
-          title: '标题1',
-          content: '随便加的一些内容'
-        },
-        {
-          head: '../../images/tian/head.png',
-          name: '帖子作者名',
-          date: '两天前',
-          images: '../../images/bg.png',
-          title: '标题1',
-          content: '随便加的一些内容'
-        },
     ],
       inputValue:'',
       reply:{},
+      reply_in:{},
+      id:'',
+      openid:'',
+      rid:'',
+      up:0,
       length:0
-
   },
 
   /**
@@ -60,28 +35,63 @@ const db = wx.cloud.database();
   onLoad: function (options) {
     
     that = this;
+    that.data.id = options.id;
+    that.data.openid = options.openid;
+    that.data.rid=options.rid;
     that.getData();
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+    that.jugdeUserLogin();
   },
 
     getData: function () {
-        db.collection('reply') .where({
+
+      // 获取回复信息
+      db.collection('reply').get({
+        success: function (res) {
+          for (var i = 0; i < res.data.length; i++) {
+            var date = res.data[i].date
+            var t1 = new Date()
+            var t = new Date(t1 - date + 16 * 3600 * 1000)
+            var d = parseInt(t.getTime() / 1000 / 3600 / 24)
+            var h = t.getHours()
+            var m = t.getMinutes()
+            if (d == 0 && h == 0 && m == 0) { res.data[i].changeDate = t.getSeconds().toString() + "秒前  "; }
+            else if (d == 0 && h == 0) { res.data[i].changeDate = m.toString() + "分钟前"; }
+            else if (d == 0) { res.data[i].changeDate = h.toString() + "小时前"; }
+            else { res.data[i].changeDate = d.toString() + "天前  "; }
+//获取赞的情况
+            var up = "up[" + i + "]";
+            db.collection('up')
+              .where({
+                _openid: that.data.openid,
+                r_id: res.data[i]._id
+              })
+              .get({
+                success: function (res2) {
+                  if (res2.data.length > 0)
+                  res.data[i].is_up=true
+                },
+              })
+            db.collection('up')
+              .where({
+                r_id: res.data[i]._id
+              })
+              .get({
+                success: function (res3) {
+                  res.data[i].up = res3.data.length
+                },
+              })
+          }
+          
+          that.setData({
+            reply: res.data,
+            length: res.data.length
+          })
+         
+        }
+      })
+        db.collection('reply_in') .where({
         }).get({
             success: function (res) {
-              
               for (var i = 0; i<res.data.length;i++)
             {
                 var date = res.data[i].date
@@ -98,13 +108,12 @@ const db = wx.cloud.database();
                 {res.data[i].changeDate = h.toString() + "小时前";}
                 else
                 {res.data[i].changeDate = d.toString() + "天前  ";}
-                console.log(res.data[i].changeDate)
             }
               that.setData({
-                reply: res.data,
-                length:res.data.length
+                reply_in: res.data,
               })
-             
+
+
             },
             fail: function (event) {
             },
@@ -118,17 +127,19 @@ const db = wx.cloud.database();
       db.collection('reply').add({
         // data 字段表示需新增的 JSON 数据
         data: {
-          head: '../../images/tian/head.png',
-          name: '帖子作者名',
           date: new Date(),
-          images: '../../images/bg.png',
-          title: '标题1',
+          user: that.data.user,
+          b_rid:that.data.bid,
+          r_id: that.data.id,
+          u_id: that.data.openid,
+          up:that.data.up,
           content: that.data.inputValue,
-          changeDate:''
+          changeDate:'',
+          is_up:false
         },
         success: function (res) {
           wx.navigateTo({
-            url: '../reply/reply',
+            url: "../reply/reply?id=" + that.data.id + "&openid=" + that.data.openid
           })
 
         },
@@ -160,4 +171,73 @@ const db = wx.cloud.database();
         url: '../meInfo/meInfo',
       })
     },
+    /**
+  * Praise 点击
+  */
+    onUpClick: function (event) {
+      var id = event.currentTarget.dataset.replyid;
+      var idx = event.currentTarget.dataset.idx;
+      if (that.data.reply[idx].is_up) {
+        // 需要判断是否存在
+        that.removeFromUpServer(id,idx);
+        that.refreshUpIcon(false,idx)
+      } else {
+        that.saveToUpServer(id,idx);
+        that.refreshUpIcon(true,idx)
+      }
+
+    },
+    /**
+   * 添加到点赞集合中
+   */
+    saveToUpServer: function (id,idx) {
+      console.log(idx)
+      console.log(that.data.reply[idx]._id)
+      db.collection('up').add({
+        data: {
+          r_id: that.data.reply[idx]._id,
+        },
+        success: function (res) {
+        },
+      })
+    },
+    /**
+  * 从点赞集合中移除
+  */
+    removeFromUpServer: function (id,idx) {
+      db.collection('up').where({
+        r_id: that.data.reply[idx].id,
+        _openid:that.data.openid
+      }).remove({
+
+      });
+    },
+    /**
+   * 刷新点赞icon
+   */
+    refreshUpIcon(isUp,idx) {
+      var up = "reply[" + idx + "].is_up";
+      that.setData({
+       [up]: isUp,
+      })
+    },
+    /**
+      * 判断用户是否登录
+      */
+    jugdeUserLogin: function (event) {
+      // 查看是否授权
+      wx.getSetting({
+        success(res) {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+            wx.getUserInfo({
+              success: function (res) {
+                that.data.user = res.userInfo;
+              }
+            })
+          }
+        }
+      })
+    },
   })
+  
